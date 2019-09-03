fb0e01bf582ebfab4cb4b8eed758824915b97120
Andrew Pirkl <andrew.pirkl@gmail.com>
2019-09-02 19:36:53 -0500

    Initial commit

---
+++ ISlowable.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public interface ISlowable {
+    event Action<float> OnSlowed;
+    event Action OnSlowEnd;
+}
