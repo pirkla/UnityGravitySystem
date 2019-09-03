dc212f9193e0db1d9bb34b44f0a71eb7cbe78e0b
Andrew Pirkl <andrew.pirkl@gmail.com>
2019-09-02 19:39:56 -0500

    Delete ISlowable.cs

--- ISlowable.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
-public interface ISlowable {
-    event Action<float> OnSlowed;
-    event Action OnSlowEnd;
-}
